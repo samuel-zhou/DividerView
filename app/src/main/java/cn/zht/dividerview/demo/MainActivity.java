4886357cd776256963660e4937c64c9819fac1c6
ZhouHaitao <samuel.zhou88@gmail.com>
2017-05-23 09:54:30 +0800

    init

--- app/src/main/java/cn/zht/dividerview/demo/MainActivity.java
+++ app/src/main/java/cn/zht/dividerview/demo/MainActivity.java
@@ -2,6 +2,7 @@ package cn.zht.dividerview.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d("MainActivity","onCreate");
     }
 }
